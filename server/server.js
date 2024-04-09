203839800d58bb6409784f5b783b4fd8f5c315d5
Bibek <zhabibek4u@gmail.com>
2024-04-09 19:30:40 +0530

    f

--- server/server.js
+++ server/server.js
@@ -124,7 +124,7 @@ app.delete('/files/:filename', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000`);
 });
